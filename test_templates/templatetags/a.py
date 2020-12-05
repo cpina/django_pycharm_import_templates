7b4b63191278ff56a8570a89c91132baa8203237
Carles Pina i Estany <carles@pina.cat>
2020-12-05 20:30:36 +0000

    Add possible workaround to help PyCharm autocompletion in templates

--- test_templates/templatetags/a.py
+++ test_templates/templatetags/a.py
@@ -1,9 +1,13 @@
 from django import template
 
+import b  # for PyCharm introspection: in a template doing '{% load a %}' to make {% say_b %} available
 
 register = template.Library()
 
-from .b import *
+
+@register.simple_tag
+def say_b(*args, **kwargs):
+    return b.say_b(*args, **kwargs)
 
 
 @register.simple_tag
